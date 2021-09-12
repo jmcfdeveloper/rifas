702d01f49fd9da4091551eaf5069846edcfaeaa9
jmcfdeveloper <80177098+jmcfveveloper@users.noreply.github.com>
2021-09-12 16:10:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,365 @@
+<?php
+
+
+
+
+
+$puesto0= array('id' => "00", 'nombre' => "Yosman CH" , 'estado' => "pagado");
+$puesto1= array('id' => "01", 'nombre' => "felipe CH" , 'estado' => "pagado");
+$puesto2= array('id' => "02", 'nombre' => "Daniela CH" , 'estado' => "comprado");
+$puesto3= array('id' => "03", 'nombre' => "Abisaí" , 'estado' => "comprado");
+$puesto4= array('id' => "04", 'nombre' => "Nanci Iglesia" , 'estado' => "comprado");
+$puesto5= array('id' => "05", 'nombre' => "nacy vacas" , 'estado' => "comprado");
+$puesto6= array('id' => "06", 'nombre' => "Beto" , 'estado' => "comprado");
+$puesto7= array('id' => "07", 'nombre' => "Sandra B" , 'estado' => "comprado");
+$puesto8= array('id' => "08", 'nombre' => "Beto" , 'estado' => "comprado");
+$puesto9=array('id' => "09", 'nombre' => "Jennifer CH" , 'estado' => "comprado");
+
+$puesto10= array('id' => "10", 'nombre' => "María vecina" , 'estado' => "comprado");
+$puesto11= array('id' => "11", 'nombre' => "Alexandra Beto" , 'estado' => "comprado");
+$puesto12= array('id' => "12", 'nombre' => "marta rosa" , 'estado' => "comprado");
+$puesto13= array('id' => "13", 'nombre' => "Diana rosa" , 'estado' => "comprado");
+$puesto14= array('id' => "14", 'nombre' => "Angelo CH" , 'estado' => "comprado");
+$puesto15= array('id' => "15", 'nombre' => "Alba Beto" , 'estado' => "comprado");
+$puesto16= array('id' => "16", 'nombre' => "Abisaí Florez" , 'estado' => "comprado");
+$puesto17= array('id' => "17", 'nombre' => "Alba Beto" , 'estado' => "comprado");
+$puesto18= array('id' => "18", 'nombre' => "Alexandra" , 'estado' => "pagado");
+$puesto19=array('id' => "19", 'nombre' => "Diana Rosa" , 'estado' => "comprado");
+
+$puesto20= array('id' => "20", 'nombre' => "Andrea CH MoM" , 'estado' => "comprado");
+$puesto21= array('id' => "21", 'nombre' => "Yesenia Rosa" , 'estado' => "comprado");
+$puesto22= array('id' => "22", 'nombre' => "abisaí Florez" , 'estado' => "comprado");
+$puesto23= array('id' => "23", 'nombre' => "Yohana Rosa" , 'estado' => "comprado");
+$puesto24= array('id' => "24", 'nombre' => "Viviana Rosa" , 'estado' => "comprado");
+$puesto25= array('id' => "25", 'nombre' => "Nanci Rosa" , 'estado' => "comprado");
+$puesto26= array('id' => "26", 'nombre' => "Vecina 13-15" , 'estado' => "comprado");
+$puesto27= array('id' => "27", 'nombre' => "Alejandra Alba" , 'estado' => "comprado");
+$puesto28= array('id' => "28", 'nombre' => "Camila Beto" , 'estado' => "comprado");
+$puesto29=array('id' => "29", 'nombre' => "Alejandra Beto" , 'estado' => "comprado");
+
+$puesto30= array('id' => "30", 'nombre' => "Yoberty CH" , 'estado' => "comprado");
+$puesto31= array('id' => "31", 'nombre' => "Lady Rangel CH" , 'estado' => "pagado");
+$puesto32= array('id' => "32", 'nombre' => "Sanda Zulia" , 'estado' => "comprado");
+$puesto33= array('id' => "33", 'nombre' => "Flor Lizcano" , 'estado' => "pagado");
+$puesto34= array('id' => "34", 'nombre' => "Andrea CH MoM" , 'estado' => "comprado");
+$puesto35= array('id' => "35", 'nombre' => "Reychel CH" , 'estado' => "pagado");
+$puesto36= array('id' => "36", 'nombre' => "María Molina" , 'estado' => "comprado");
+$puesto37= array('id' => "37", 'nombre' => "Diana Rosa" , 'estado' => "comprado");
+$puesto38= array('id' => "38", 'nombre' => "Andrea Ch" , 'estado' => "comprado");
+$puesto39=array('id' => "39", 'nombre' => "Yuri CH " , 'estado' => "comprado");
+
+$puesto40= array('id' => "40", 'nombre' => "Diana-Jhonatan" , 'estado' => "pagado");
+$puesto41= array('id' => "41", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto42= array('id' => "42", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto43= array('id' => "43", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto44= array('id' => "44", 'nombre' => "Oración" , 'estado' => "comprado");
+$puesto45= array('id' => "45", 'nombre' => "Reychel Lalo" , 'estado' => "pagado");
+$puesto46= array('id' => "46", 'nombre' => "name" , 'estado' => "comprado");
+$puesto47= array('id' => "47", 'nombre' => "DeyaRosa" , 'estado' => "comprado");
+$puesto48= array('id' => "48", 'nombre' => "Laudith" , 'estado' => "comprado");
+$puesto49=array('id' => "49", 'nombre' => "Chiqui CH" , 'estado' => "comprado");
+
+$puesto50= array('id' => "50", 'nombre' => "Contento CH" , 'estado' => "comprado");
+$puesto51= array('id' => "51", 'nombre' => "Diego CH" , 'estado' => "pagado");
+$puesto52= array('id' => "52", 'nombre' => "Fani Joseito" , 'estado' => "comprado");
+$puesto53= array('id' => "53", 'nombre' => "Lalo lula" , 'estado' => "comprado");
+$puesto54= array('id' => "54", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto55= array('id' => "55", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto56= array('id' => "56", 'nombre' => "Angelo CH" , 'estado' => "comprado");
+$puesto57= array('id' => "57", 'nombre' => "Gonzalo F" , 'estado' => "comprado");
+$puesto58= array('id' => "58", 'nombre' => "Andrea CH" , 'estado' => "comprado");
+$puesto59=array('id' => "59", 'nombre' => "Andrea Ch" , 'estado' => "comprado");
+
+$puesto60= array('id' => "60", 'nombre' => "María Martinez" , 'estado' => "comprado");
+$puesto61= array('id' => "61", 'nombre' => "Zulia " , 'estado' => "comprado");
+$puesto62= array('id' => "62", 'nombre' => "Maira FLorez CH" , 'estado' => "comprado");
+$puesto63= array('id' => "63", 'nombre' => "Yoana Rosa" , 'estado' => "comprado");
+$puesto64= array('id' => "64", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto65= array('id' => "65", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto66= array('id' => "66", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto67= array('id' => "67", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto68= array('id' => "68", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto69=array('id'  => "69", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+
+
+$puesto70= array('id' => "70", 'nombre' => "Andrea CH" , 'estado' => "comprado");
+$puesto71= array('id' => "71", 'nombre' => "Andrea CH" , 'estado' => "comprado");
+$puesto72= array('id' => "72", 'nombre' => "LAudith FLorez" , 'estado' => "comprado");
+$puesto73= array('id' => "73", 'nombre' => "Gonzalo Florez" , 'estado' => "comprado");
+$puesto74= array('id' => "74", 'nombre' => "Alba Soto" , 'estado' => "comprado");
+$puesto75= array('id' => "75", 'nombre' => "Fani Zulia" , 'estado' => "comprado");
+$puesto76= array('id' => "76", 'nombre' => "XXXX" , 'estado' => "comprado");
+$puesto77= array('id' => "77", 'nombre' => "Oración" , 'estado' => "comprado");
+$puesto78= array('id' => "78", 'nombre' => "Lusila DEYO" , 'estado' => "comprado");
+$puesto79= array('id' => "79", 'nombre' => "Gonzalo FLorez" , 'estado' => "comprado");
+
+
+
+$puesto80= array('id' => "80", 'nombre' => "Andrea CH T" , 'estado' => "comprado");
+$puesto81= array('id' => "81", 'nombre' => "Andrea CH V" , 'estado' => "comprado");
+$puesto82= array('id' => "82", 'nombre' => "Andrea CH N" , 'estado' => "comprado");
+$puesto83= array('id' => "83", 'nombre' => "Andrea CH V" , 'estado' => "comprado");
+$puesto84= array('id' => "84", 'nombre' => "Reychel Lalo" , 'estado' => "pagado");
+$puesto85= array('id' => "85", 'nombre' => "name" , 'estado' => "comprado");
+$puesto86= array('id' => "86", 'nombre' => "name" , 'estado' => "comprado");
+$puesto87= array('id' => "87", 'nombre' => "name" , 'estado' => "comprado");
+$puesto88= array('id' => "88", 'nombre' => "name" , 'estado' => "comprado");
+$puesto89= array('id' => "89", 'nombre' => "name" , 'estado' => "comprado");
+
+$puesto90= array('id' => "90", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto91= array('id' => "91", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto92= array('id' => "92", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto93= array('id' => "93", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto94= array('id' => "94", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto95= array('id' => "95", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto96= array('id' => "96", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto97= array('id' => "97", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto98= array('id' => "98", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+$puesto99= array('id' => "99", 'nombre' => "Diana-Jhonatan" , 'estado' => "comprado");
+
+
+
+
+
+
+
+  
+$puestos = array(
+'00'=>$puesto0 ,
+'01'=>$puesto1 ,
+'02'=>$puesto2 ,
+'03'=>$puesto3 ,
+'04'=>$puesto4 ,
+'05'  =>$puesto5,
+'06'  =>$puesto6,
+'07'  =>$puesto7,
+'08'  =>$puesto8,
+'09'  =>$puesto9,
+'10'  =>$puesto10,
+
+
+'11'  =>$puesto11,
+'12'  =>$puesto12,
+'13'  =>$puesto13,
+'14'  =>$puesto14,
+'15'  =>$puesto15,
+'16'  =>$puesto16,
+'17'  =>$puesto17,
+'18'  =>$puesto18,
+'19'  =>$puesto19,
+'20'  =>$puesto20,
+'21'  =>$puesto21,
+'22'  =>$puesto22,
+'23'  =>$puesto23,
+'24'  =>$puesto24,
+'25'  =>$puesto25,
+'26'  =>$puesto26,
+'27'  =>$puesto27,
+'28'  =>$puesto28,
+'29'  =>$puesto29,
+'30'  =>$puesto30,
+'31'  =>$puesto31,
+'32'  =>$puesto32,
+'33'  =>$puesto33,
+'34'  =>$puesto34,
+'35'  =>$puesto35,
+'36'  =>$puesto36,
+'37'  =>$puesto37,
+'38'  =>$puesto38,
+'39'  =>$puesto39,
+'40'  =>$puesto40,
+'41'  =>$puesto41,
+'42'  =>$puesto42,
+'43'  =>$puesto43,
+'44'  =>$puesto44,
+'45'  =>$puesto45,
+'46'  =>$puesto46,
+'47'  =>$puesto47,
+'48'  =>$puesto48,
+'49'  =>$puesto49,
+'50'  =>$puesto50,
+'51'  =>$puesto51,
+'52'  =>$puesto52,
+'53'  =>$puesto53,
+'54'  =>$puesto54,
+'55'  =>$puesto55,
+'56'  =>$puesto56,
+'57'  =>$puesto57,
+'58'  =>$puesto58,
+'59'  =>$puesto59,
+'60'  =>$puesto60,
+'61'  =>$puesto61,
+'62'  =>$puesto62,
+'63'  =>$puesto63,
+'64'  =>$puesto64,
+'65'  =>$puesto65,
+'66'  =>$puesto66,
+'67'  =>$puesto67,
+'68'  =>$puesto68,
+'69'  =>$puesto69,
+'70'  =>$puesto70,
+'71'  =>$puesto71,
+'72'  =>$puesto72,
+'73'  =>$puesto73,
+'74'  =>$puesto74,
+'75'  =>$puesto75,
+'76'  =>$puesto76,
+'77'  =>$puesto77,
+'78'  =>$puesto78,
+'79'  =>$puesto79,
+'80'  =>$puesto80,
+'81'  =>$puesto81,
+'82'  =>$puesto82,
+'83'  =>$puesto83,
+'84'  =>$puesto84,
+'85'  =>$puesto85,
+'86'  =>$puesto86,
+'87'  =>$puesto87,
+'88'  =>$puesto88,
+'89'  =>$puesto89,
+'90'  =>$puesto90,
+'91'  =>$puesto91,
+'92'  =>$puesto92,
+'93'  =>$puesto93,
+'94'  =>$puesto94,
+'95'  =>$puesto95,
+'96'  =>$puesto96,
+'97'  =>$puesto97,
+'98'  =>$puesto98,
+'99'  =>$puesto99
+
+
+);
+/* 
+
+
+); */
+
+
+ 
+
+
+ 
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.82.0">
+    <title>Checkout example · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+   <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4" src="images/icon-money.svg" alt="" width="72" height="57">
+      <h2>Rifa Relojes</h2>
+      <p class="lead">Puestos</p>
+    </div>
+
+
+
+
+  <div class="row">
+
+    <?php  foreach ($puestos as &$puesto) { 
+      
+      if($puesto['estado']=="comprado"){
+        $badge="badge bg-secondary";
+      }else if($puesto['estado']=="pagado"){
+        $badge="badge bg-success";
+      }
+      
+      ?>
+    
+
+
+   
+
+     
+      <div class="col-4 col-sm-3">
+     
+
+        <div class="card w-60">
+          <div class="card-body">
+            <h5 class="card-title"> <strong><?php echo $puesto['id']?></strong> <?php echo " ".$puesto['nombre']?> </h5>
+            <p class="card-text">  <span class="<?php echo  $badge;?>"><?php echo $puesto['estado']?></span></p>
+           
+          </div>
+        </div>
+     
+      </div>
+
+      
+    
+    
+    <?php }  ?>
+
+ 
+
+
+  </div>
+
+
+
+
+
+
+
+  </main>
+
+
+
+
+
+
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2017–2021 Company Name</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+      <script src="form-validation.js"></script>
+  </body>
+</html>
